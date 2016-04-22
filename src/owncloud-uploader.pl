ae00f64ccd8da042c4f815e92cdd8696c4a37844
Håvard Moen <post@haavard.name>
2016-04-22 21:37:52 +0200

    changed realm used by newer owncloud

--- src/owncloud-uploader.pl
+++ src/owncloud-uploader.pl
@@ -156,7 +156,7 @@ sub do_picture_upload {
     -user => $conf->param('owncloud_user'),
     -pass => $conf->param('owncloud_password'),
     -url => $url,
-    -realm => 'ownCloud'
+    -realm => 'sabre/dav'
   );
   unless($dav->open(-url => $url)) {
     $logger->error("Unable to open $url: " . $dav->message);
